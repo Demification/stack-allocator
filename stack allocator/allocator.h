f258f8d8cd4baac04740c91735c9515d31c3b28c
Vredinka <root.faust@gmail.com>
2019-03-26 00:29:37 +0200

    f1

---
+++ stack allocator/allocator.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <windows.h>
+#include <malloc.h>
+
+namespace alc
+{
+	inline static void PAUSE();
+
+	class allocator
+	{
+	private:
+		size_t masterinc;
+
+		static unsigned long __stdcall thread_func(void* lpParameter);
+
+	public:
+		UINT sizebytes;
+
+		struct parameters
+		{
+			void* ptr = nullptr;
+			size_t size;
+			HANDLE handle;
+		} param;
+
+		struct block
+		{
+			const void *ptr;
+			size_t master;
+		};
+
+		const void *init(const size_t blocksize, const size_t blockcount);
+
+		const void *init(const size_t bytes);
+
+		void destroy();
+
+		block* allocate();
+
+		block* allocate(const size_t block_count);
+
+		void free(block* pBlock);
+
+		void allfree();
+
+		const block* getblock(const size_t index);
+
+		void* malloc(size_t bytes);
+
+		void free(const void* ptr);
+
+		void* get0ptr();
+
+	protected:
+		block *b0, *b1;
+		const block *blocks;
+		size_t blockcount;
+		size_t blocksize;
+	};
+
+}
